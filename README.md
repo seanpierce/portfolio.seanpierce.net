bd1539b15dafddf6afc787f0791ad22b75e6998c
seanpierce <sumler.sean@gmail.com>
2026-06-01 14:14:07 -0700

    Initialize repository

---
+++ README.md
@@ -0,0 +1,1 @@
+# portfolio.seanpierce.net
---
+++ index.js
@@ -0,0 +1,23 @@
+console.log(`
+███████╗███████╗ █████╗ ███╗   ██╗
+██╔════╝██╔════╝██╔══██╗████╗  ██║
+███████╗█████╗  ███████║██╔██╗ ██║
+╚════██║██╔══╝  ██╔══██║██║╚██╗██║
+███████║███████╗██║  ██║██║ ╚████║
+╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
+Hey there, curious developer 👋
+
+Built by Sean Sumler
+https://yourwebsite.com
+`);
+
+console.log(
+  '%cSean Sumler',
+  'font-size:32px;font-weight:bold;'
+);
+
+console.log(
+  '%cCreative Technologist & Software Developer',
+  'font-size:16px;'
+);
