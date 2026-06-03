7a3e84916c888e2cc3b43b8b8531f867ac2a4d6e
seanpierce <sumler.sean@gmail.com>
2026-06-03 12:16:20 -0700

    Create initial reader view styles and functions

--- index.js
+++ index.js
@@ -37,4 +37,49 @@ console.log(
 
 const goTo = (link) => {
     window.open(link, '_blank');
-}
+};
+
+const toggleReaderView = () => {
+  const isReaderViewEnabled = document.body.classList.contains('reader-view');
+
+  if (isReaderViewEnabled) {
+    document.body.classList.remove('reader-view');
+    hideImageAltInDesignView();
+  } else {
+    document.body.classList.add('reader-view');
+    showImageAltInReaderView();
+  }
+};
+
+const enableReaderView = () => {
+  if (!document.body.classList.contains('reader-view')) {
+    toggleReaderView();
+  }
+};
+
+const showImageAltInReaderView = () => {
+  document.querySelectorAll('img.show-alt-in-reader-view').forEach(img => {
+    const caption = document.createElement('h3');
+    caption.classList.add('reader-img-alt-caption');
+    caption.textContent = img.alt;
+    img.after(caption);
+  });
+};
+
+const hideImageAltInDesignView = () => {
+  document.querySelectorAll('h3.reader-img-alt-caption').forEach(h3 => {
+    h3.remove();
+  });
+};
+
+const initReaderViewFromQuery = () => {
+  const params = new URLSearchParams(window.location.search);
+  const readerview = params.get('readerview');
+  const isEnabled = readerview?.toLowerCase() === 'true' || readerview === '1';
+
+  if (params.has('readerview') && isEnabled) {
+    enableReaderView();
+  }
+};
+
+initReaderViewFromQuery();
